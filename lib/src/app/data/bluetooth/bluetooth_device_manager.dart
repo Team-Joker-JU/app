0c29a5a93aec03d66928a63fcf206bcbe794e698
Warcaith <f.v.carlsson@gmail.com>
2021-05-04 01:59:16 +0200

    feat: add 'FlutterBlue' wrapping

--- lib/src/app/data/bluetooth/bluetooth_device_manager.dart
+++ lib/src/app/data/bluetooth/bluetooth_device_manager.dart
@@ -1,58 +1,32 @@
+import 'dart:async';
+
 import 'package:flutter_blue/flutter_blue.dart';
-import 'package:ims/src/app/data/bluetooth/containers/robot-container.dart';
-import 'package:ims/src/app/data/bluetooth/gatt-container.dart';
-import 'package:ims/src/app/data/bluetooth/gatt_resolver.dart';
-import 'package:ims/src/app/data/robot/robot_controller.dart';
-import 'package:ims/src/util/gatt_characteristics.dart';
-import 'package:ims/src/util/gatt_services.dart';
+import 'package:flutter_blue/gen/flutterblue.pbjson.dart';
+import 'package:ims/src/app/data/bluetooth/bluetooth_interactor.dart';
+import 'package:ims/src/app/data/bluetooth/bluetooth_controller.dart';
 
-class BluetoothDeviceManager<T extends GATTContainer> {
-  BluetoothDevice? _device;
-  GATTContainer? _container;
+class BluetoothDeviceManager<Interactor extends BluetoothInteractor, Controller extends BluetoothController> {
+  late BluetoothDevice _device;
+  late Interactor _interactor;
+  late Controller _controller;
 
-  BluetoothDevice? get device => _device;
-  GATTContainer? get container => _container;
+  BluetoothDevice get device => _device;
+  Interactor get interactor => _interactor;
+  Controller get controller => _controller;
+  
+  /// Find a specific [BluetoothService] by [uuid] among a list of [services].
+  /// Throw a [TimeoutException]
+  Future<void> connect(BluetoothDevice device, {Duration timeout = const Duration(seconds: 10)}) async {
+    // Pull request fix: https://github.com/pauldemarco/flutter_blue/pull/362
+    await device.connect().timeout(timeout, onTimeout: () => { throw TimeoutException("Timeout on device connect.")});
 
-  Future<bool> connect(BluetoothDevice device) async {
-    await device.connect(timeout: Duration(seconds: 10), autoConnect: false);
-
-    device.state.listen((event) async {
-      switch (event) {
-        case BluetoothDeviceState.connected:
-          print("NU");
-          break;
-
-        case BluetoothDeviceState.disconnected:
-          print("disconnected");
-          break;
-        default:
-          break;
-      }
-    });
-
-    bool connected = (await FlutterBlue.instance.connectedDevices).contains(device);
-
-    if (connected) {
-      this._device = device;
-      this._container = GATTContainer.createInstance(T, await device.discoverServices())!;
-      print(this._container?.servicesByUUID);
-      print(this._container?.characteristicsByUUID);
-      print(FlutterBlue.instance.connectedDevices);
-      //var service = GATTResolver.findService(await device.discoverServices(), ROBOT_UUID)!;
-      //await GATTResolver.findCharacteristic(service, ROBOT_ACCELERATION_UUID)!.write([0x00]);
-
-      //await characteristic.write([0x00]);
-
-      var controller = RobotController(_container as RobotContainer);
-      await controller.handshake();
-    }
-
-    return connected;
+    final services = await device.discoverServices();
+    _device = device;
+    _interactor = BluetoothInteractor.createInstance<Interactor>(services)!;
+    _controller = BluetoothController.createInstance<Controller, Interactor>(_interactor)!;
   }
 
   Future<void> disconnect() async {
-    await this._device?.disconnect();
-    this._device = null;
-    this._container = null;
+    return _device.disconnect();
   }
 }
--- lib/src/app/data/bluetooth/bluetooth_discover_manager.dart
+++ lib/src/app/data/bluetooth/bluetooth_discover_manager.dart
@@ -1,26 +1,15 @@
 import 'package:flutter_blue/flutter_blue.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class BluetoothDiscoverManager {
   Stream<BluetoothState> get state => FlutterBlue.instance.state;
   Stream<List<ScanResult>> get scanResults => FlutterBlue.instance.scanResults.asBroadcastStream();
-
-  Future<dynamic> startScan({required Duration timeout, ScanMode scanMode = ScanMode.lowLatency}) {
+  
+  Future<dynamic> startScan({required Duration timeout, ScanMode scanMode = ScanMode.balanced}) async {
     return FlutterBlue.instance.startScan(timeout: timeout, scanMode: scanMode);
   }
 
   Future<dynamic> stopScan() async {
-    FlutterBlue.instance.stopScan();
-  }
-
-  Future<List<BluetoothDevice>> devices(Duration timeout) async {
-    List<BluetoothDevice> devices = [];
-
-    await FlutterBlue
-        .instance
-        .scan(timeout: timeout)
-        .listen((event) => devices.add(event.device))
-        .asFuture();
-    
-    return devices;
+    return FlutterBlue.instance.stopScan();
   }
 }
