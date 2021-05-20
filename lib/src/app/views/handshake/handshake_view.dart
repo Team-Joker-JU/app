4d97cecdeaf55084a919dd84259e1339f23d3b9c
Emil Persson <emilper1999@gmail.com>
2021-05-20 15:16:38 +0200

    added handshake animation

--- lib/src/app/views/handshake/handshake_view.dart
+++ lib/src/app/views/handshake/handshake_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:ims/src/app/view-models/bluetooth_view_model.dart';
 import 'package:ims/src/app/view-models/handshake_view_model.dart';
 import 'package:stacked/stacked.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class HandshakeView extends ViewModelBuilderWidget<HandshakeViewModel> {
   @override
@@ -32,7 +33,40 @@ class HandshakeView extends ViewModelBuilderWidget<HandshakeViewModel> {
         backgroundColor: Colors.blueAccent,
         actions: <Widget>[],
       ),
-      body: Text("Hej"),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            child: SpinKitPulse(
+              duration: Duration(milliseconds: 1200),
+              color: Colors.blue,
+              size: 500,
+            ),
+          ),
+          Container(
+            child: SpinKitPulse(
+              duration: Duration(milliseconds: 1200),
+              color: Colors.blue,
+              size: 300,
+            ),
+          ),
+          Center(
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.white
+                ),
+                borderRadius: BorderRadius.all(Radius.circular(75)),
+                color: Colors.white,
+              ),
+              child: Icon(
+                Icons.bluetooth,
+                size: 175,
+                color: Colors.blue,
+              ),
+            ),
+          ),
+        ],
+      )
     );
   }
 }
